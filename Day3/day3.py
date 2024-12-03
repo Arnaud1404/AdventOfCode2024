e8b4687fd58f313e02651ed25847a76dca38a0e9
Arnaud T480s <arnaudgomes14@gmail.com>
2024-12-03 13:53:37 +0100

    Day 3 part 2 ideas

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,11 +1,13 @@
 # Part 1
 
-with open("input.txt", 'r') as f:
-    # i = Reading head
-    data:str = f.read()
-    i = 0
+# Evaluates the data from a given start and end index
+# Returns the result between the start and end index
+def eval(data:str, start:int, end:int) -> int:
+    assert start < end
+    assert end <= len(data)
+    i = start
     result = 0
-    while i < len(data):
+    while i < len(data) and i < end:
         i = data.find('mul(', i)
         # No more mul( from i onwards
         if i == -1:
@@ -28,7 +30,41 @@ with open("input.txt", 'r') as f:
         if data[i] == ")":
             result += int(arg1) * int(arg2)
         i += 1
-    print(result) # 174960292
+    return result 
+
+with open("example.txt", 'r') as f:
+    data:str = f.read()
+    # i = Reading head
+    result1 = eval(data, 0, len(data)) 
+    print(result1) # 174960292
+
+    enabled = True
+    index = 0
+    result2 = 0
+    end = len(data) - 1
+    while index < len(data):
+        if enabled:
+            end = data.find("don't()", index) + len("don't()")
+        else:
+            end = data.find("do()", index)+ len("do()")
+        if end != -1:
+            # found an instruction
+            if enabled:
+                result2 += eval(data, index, end)
+            enabled = not enabled
+            
+        else: #EOF
+            if enabled:
+                result2 += eval(data, index, len(data))
+            end = len(data) - 1
+        # Update index
+        if enabled:
+            index = end 
+        else:
+            i = end 
+    print(result2)
+
+    
 
 # Part 2
 
