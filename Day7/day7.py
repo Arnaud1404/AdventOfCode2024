ca1a2496e85c7af132d9508400f3ad5822fb9f51
Arnaud <arnaudgomes14@gmail.com>
2024-12-08 20:37:06 +0100

    Day 7 finally

--- Day7/day7.py
+++ Day7/day7.py
@@ -1,13 +1,25 @@
+def explore(target, nums):
+    if target == 0:
+        return True
+    if len(nums) == 0:
+        return False
+    # Try division only if remainder is zero
+    return (target % nums[0] == 0 and explore(target // nums[0], nums[1:]))\
+            or explore(target - nums[0], nums[1:])
+
+
 def evaluate(expr) -> int:
     target, nums = expr.split(":")
     target = int(target)
-    print(nums)
     nums = [int(num) for num in nums.split()]
-    
+    # Do the operations in reverse order
+    if explore(target, nums[::-1]):
+        return target
+    return 0
 
-with open("example.txt") as f:
+with open("input.txt") as f:
     data = f.read().split("\n")
     result = 0
     for expr in data:
         result += evaluate(expr)
-    print(result)
+    print(f"\nFinal result = {result}")
