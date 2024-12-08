30c2814b4e39dee6e65e5d890a3c404142fba2c5
Arnaud <arnaudgomes14@gmail.com>
2024-12-08 23:48:41 +0100

    Day 8 part 1

---
+++ Day8/day8.py
@@ -0,0 +1,41 @@
+def offset(coord1, coord2):
+    x1, y1 = coord1
+    x2, y2 = coord2
+    return (x1-x2, y1-y2)
+
+with open("input.txt") as f:
+    data = f.read().split()
+    data = [list(line) for line in data]
+    n = len(data) # Always a square matrix
+    antennas = {} # key = antenna:str
+                  # value = List of each coordinates (r:int,c:int)
+    antinodes = set() # Set of coordinates, no dupes allowed
+    
+    for r in range(n):
+        for c in range(n):
+            if data[r][c] != ".":
+                antenna = data[r][c]
+                antennas.setdefault(antenna, []).append((r,c))
+
+    for antenna, coords in antennas.items():
+        for i in range(len(coords)):
+            for j in range(i + 1, len(coords)):
+                dx, dy = offset(coords[i], coords[j])
+                x1, y1 = coords[i]
+                x2, y2 = coords[j]
+
+                new_x, new_y = x1+dx, y1+dy
+                if 0 <= new_x < n and 0 <= new_y < n:
+                    antinodes.add((new_x, new_y))
+                    if data[new_x][new_y] == ".":
+                        data[new_x][new_y] = "#"
+
+                new_x, new_y = x2-dx, y2-dy
+                if 0 <= new_x < n and 0 <= new_y < n:
+                    antinodes.add((new_x, new_y))
+                    if data[new_x][new_y] == ".":
+                        data[new_x][new_y] = "#"
+    print(data)
+    print(len(antinodes))
+                
+            
